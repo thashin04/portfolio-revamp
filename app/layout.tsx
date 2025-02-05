6848a13e1d6b22b8893b47f8c60be9ad1013ca49
thashin04 <thashin2004@gmail.com>
2025-02-05 09:45:56 -0500

    metadata stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,24 @@
-'use client'; 
-
+import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { motion, AnimatePresence } from "framer-motion";
-import { usePathname } from "next/navigation";
-import Head from "next/head"; 
+import PageTransition from "../components/PageTransition";
+
+export const metadata: Metadata = {
+  title: "Thashin Bhuiyan",
+  description: "Welcome to my portfolio website, showcasing various creative and technical projects.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-  
   return (
     <html lang="en">
-      <Head>
-        <title>Thashin Bhuiyan</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Welcome to my portfolio website, showcasing various creative and technical projects." />
-      </Head>
       <body>
         <Navbar />
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={pathname}
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.7 }}
-          >
-            {children}
-          </motion.div>
-        </AnimatePresence>
+        <PageTransition>{children}</PageTransition> {/* Now inside the Client Component */}
         <Footer />
       </body>
     </html>
